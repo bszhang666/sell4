eba968d375c7e68e0354e31a457ad9892fd4d5b9
bszhang666 <bs_zhang2022@163.com>
2022-12-26 18:58:32 +0800

    first

---
+++ src/main/java/com/imooc/sell4/Sell4Application.java
@@ -0,0 +1,13 @@
+package com.imooc.sell4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sell4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sell4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/sell4/Sell4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.sell4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sell4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
